9b5414a50c0504d87a4595ccccbf1373c0f5d663
rony <murony59@gmail.com>
2025-05-09 23:10:50 +0600

    added the initial files

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.get('/', (req: Request, res: Response) => { 
+  res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+  console.log('Server is running on http://localhost:3000');
+});
